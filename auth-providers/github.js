906e4e6d5163795723c6f338cf78c69e28b486d3
formula1 <samtobia@gmail.com>
2015-03-04 18:25:43 -0800

    encoding uri component

--- auth-providers/github.js
+++ auth-providers/github.js
@@ -4,8 +4,8 @@ module.exports = {
   name:"github",
   authLocation: function(client_id,state,redirect_uri){
     return "https://github.com/login/oauth/authorize" +
-      "?client_id="+client_id +
-      "&state="+state +
-      "&redirect_uri="+redirect_uri;
+      "?client_id="+encodeURIComponent(client_id) +
+      "&state="+encodeURIComponent(state)+
+      "&redirect_uri="+encodeURIComponent(redirect_uri);
   }
 };
